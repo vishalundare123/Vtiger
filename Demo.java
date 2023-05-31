1be09fecffd55b11a040818227ad9b01d5f8a2de
vishalundare123 <130492895+vishalundare123@users.noreply.github.com>
2023-05-31 09:05:36 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,64 @@
+
+class demo
+
+{
+    int a=0;
+    static {
+        System.out.println("hello");
+    }
+    {
+        System.out.println("non static block");
+    }
+    {
+        System.out.println("non static block");
+    }
+    public  static void main (String[] args)
+    {
+        System.out.println("hello wolrld");
+        demo a1=new demo();
+        }
+        static {
+            System.out.println("bye bye");
+        }
+
+}
+ 
+ 
+class demo1
+{
+    int a=0;
+    static {
+        System.out.println("hello");
+    }
+    {
+        System.out.println("non static block");
+    }
+    {
+        System.out.println("non static block");
+    }
+    public  static void main (String[] args)
+    {
+        System.out.println("hello wolrld");
+        demo1 a1=new demo1();
+        demo1 a2=new demo1();
+        
+        
+            System.out.println("bye bye");
+    }  
+
+}
+
+import java.util.Scanner;
+class Demo2{
+    String username;
+    int pass; 
+    public m1{
+ Scanner sc=new scanner(Syetm.in); 
+    }
+     {
+    Scanner sc=new Scanner(System.in);
+     }
+    public static void main(String[]args){
+     f1 a=new
+    }
+}
